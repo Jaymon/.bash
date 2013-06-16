00eadcb384d055d1497bf65a0ff5437c26ed3e49
Jay Marcyes <jay@marcyes.com>
2013-06-15 18:14:36 -0700

    adds a vagrant helper addon that will install the sshv command for vagrant so you can easily ssh into a started ssh box

--- addon.sh
+++ addon.sh
@@ -9,13 +9,40 @@
 # http://apple.stackexchange.com/questions/55875/have-git-autocomplete-branches-at-the-terminal-command-line/55886#55886
 # http://code-worrier.com/blog/autocomplete-git/
 #? addGitTabComplete -> run this to add git tab auto complete to your shell
-function addGitTabComplete(){
+function addGitTabComplete() {
   addAddon "git_completion" "Git AutoComplete" "https://raw.github.com/git/git/master/contrib/completion/git-completion.bash"
 }
 
 #? addLc -> run this to add lc command to your shell
-function addLc(){
+function addLc() {
   addAddon "listcommands" "List Commands" "https://raw.github.com/Jaymon/lc-listcommands-bash/master/listcommands.sh"
+}
+
+#? addVagrantHelper -> adds a vagrant helper file with handy vagrant stuff
+function addVagrantHelper() {
+  echo "Finding vagrant public key (this could take some time)..."
+  vagrant_pub_key=$(find / -name vagrant.pub 2>/dev/null | grep "vagrant.pub")
+  if [[ -n $vagrant_pub_key ]]; then
+    vagrant_priv_key=${vagrant_pub_key:0:-4}
+    echo "vagrant private key found at $vagrant_priv_key"
+
+    vagrant_ssh_command='ssh -A -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vagrant@localhost -p 2222'
+
+    bash_dir=$(getBashDir)
+    bash_f=$bash_dir/addon_vagrant.sh
+
+    echo "Installing $bash_f"
+    echo "alias sshv='$vagrant_ssh_command -i $vagrant_priv_key'" > $bash_f
+
+    if [ -f $bash_f ]; then
+      . $bash_f
+      echo "vagrant addon installed!"
+    fi
+
+  else
+    echo "key was not found!!!, is vagrant installed?"
+  fi
+
 }
 
 function addAddon() {
