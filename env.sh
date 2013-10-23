4fb9d0b1b2f78348c963c5895b1c34a46e89cbbc
Jay Marcyes <jay@marcyes.com>
2013-10-23 16:00:51 -0700

    messed with terminal title a bit

--- env.sh
+++ env.sh
@@ -79,10 +79,12 @@ PS1="$PS1"'\[$(color=$GREEN; if [[ $RET -gt 0 ]]; then color=$RED; fi; echo -ne 
 # If this is an xterm set the title to user@host:dir
 case "$TERM" in
     xterm*|rxvt*|Eterm|aterm|kterm|gnome*)
-        PS1="$PS1"'\[\033]0;\u@\h:'"${chroot}"'${PWD}\007\]'
+        #PS1="$PS1"'\[\033]0;\u@\h:'"${chroot}"'${PWD}\007\]'
+        PS1="$PS1"'\[\033]0;\u@'"${chroot}"'${PWD##*/}\007\]'
         ;;
     screen)
-        PS1="$PS1"'\[\033_\u@\h:'"${chroot}"'${PWD}\033\\\'
+        #PS1="$PS1"'\[\033_\u@\h:'"${chroot}"'${PWD}\033\\\'
+        PS1="$PS1"'\[\033_\u@'"${chroot}"'${PWD##*/}\033\\\'
 ;;
 esac
 
