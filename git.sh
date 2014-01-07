cfb7e19cfb2a356a5613a997abf73e8a703d3a23
Jay Marcyes <jay@marcyes.com>
2014-01-06 18:04:48 -0800

    adds some more git commands

--- git.sh
+++ git.sh
@@ -1,6 +1,16 @@
 alias gc="git commit -a -m"
 alias ga="git add"
 alias gr="git checkout --"
-alias gp="git pull origin master"
-alias glo='git log --pretty=format:"%h%x09%an%x09%s"'
+alias gs='git status'
+
+alias gp='git push'
+alias gpo='git push origin'
+alias gpom='git push origin master'
+
+alias gl='git pull'
+alias glo='git pull origin'
+alias glom="git pull origin master"
+
+alias glog='git log --pretty=format:"%h%x09%an%x09%s"'
 alias gamend="git commit --amend -m"
+
