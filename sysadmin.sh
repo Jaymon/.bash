0c4f16aaf31400572b7f7964bf1e023f0a699bec
Jay Marcyes <jay@marcyes.com>
2014-09-11 00:42:34 -0700

    adds easier git pushing and pulling commands fro branches

--- sysadmin.sh
+++ sysadmin.sh
@@ -7,10 +7,19 @@ alias gd='git diff'
 alias gp='git push'
 alias gpo='git push origin'
 alias gpom='git push origin master'
+alias gb='git branch 2> /dev/null | grep -e ^* | cut -d" " -f 2'
+
+function gpob () {
+  git push origin $(gb)
+}
 
 alias gl='git pull'
 alias glo='git pull origin'
 alias glom="git pull origin master"
+
+function glob () {
+  git pull origin $(gb)
+}
 
 alias glog='git log --pretty=format:"%h%x09%an%x09%s"'
 alias gamend="git commit --amend -m"
@@ -27,3 +36,4 @@ function cs() {
   grep --color=auto --exclude=*.pyc -Rin "$1" *
 }
 alias sc=cs
+
