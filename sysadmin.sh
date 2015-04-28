387808be7f58caebd7fc566f81124d203f73d306
Jay Marcyes <jay@marcyes.com>
2015-04-28 13:30:10 -0700

    adds a gf file under source control command to the sysadmin script

--- sysadmin.sh
+++ sysadmin.sh
@@ -1,3 +1,6 @@
+###############################################################################
+# Git specific helpers
+###############################################################################
 alias gc="git commit -a -m"
 alias ga="git add"
 alias gr="git checkout --"
@@ -26,17 +29,39 @@ alias gamend="git commit --amend -m"
 # git log --graph --decorate --all
 alias gg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n'' %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
 
+#? gf [FILENAME] -> search all the files under source control in the current folder
+# http://stackoverflow.com/questions/15606955/how-can-i-make-git-show-a-list-of-the-files-that-are-being-tracked
+function gf () {
+  if [[ $# -eq 0 ]]; then
+    git ls-tree -r $(gb) --name-only
+  else
+    git ls-tree -r $(gb) --name-only | grep "$1"
+  fi
+
+}
+
+
+###############################################################################
+# Vagrant specific helpers
+###############################################################################
+
 alias vu='vagrant up'
 alias vp='vagrant provision'
 alias vd='vagrant destroy -f'
 alias vs='vagrant suspend'
 alias vr='vagrant reload'
 
+
+###############################################################################
+# Misc helpers
+###############################################################################
+
 #? cs INPUT -> search all the files in current folder
 function cs() {
   grep --color=auto --exclude=*.pyc -Rin "$1" *
 }
 alias sc=cs
+alias sf=cs
 
 
 #? ush -> unsecured ssh, this is handy for AWS and other boxes where the box's host key can change
