a641f711ad0392a535ddb725d8ff4fdb85097568
Jay Marcyes <jay@marcyes.com>
2017-03-10 14:04:39 -0800

    moves extract into its own file

--- util.sh
+++ util.sh
@@ -478,28 +478,28 @@ function flushdns() {
 #? extract FILEPATH -> figure out how to extract a file and extract it
 # http://natelandau.com/my-mac-osx-bash_profile/
 # Jarid showed me this, magical
-function extract () {
-  if [[ -f $1 ]]; then
-    case "$1" in
-      *.tar.bz2)   tar xjf "$1"     ;;
-      *.tar.gz)    tar xzf "$1"     ;;
-      *.bz2)       bunzip2 "$1"     ;;
-      *.rar)       unrar e "$1"     ;;
-      *.r00)       unrar e "$1"     ;;
-      *.gz)        gunzip "$1"      ;;
-      *.tar)       tar xf "$1"      ;;
-      *.tbz2)      tar xjf "$1"     ;;
-      *.tgz)       tar xzf "$1"     ;;
-      *.zip)       unzip "$1"       ;;
-      *.Z)         uncompress "$1"  ;;
-      *.7z)        7z x "$1"        ;;
-      *.001)       7z x "$1"        ;;
-      *)     echo "'$1' cannot be extracted via extract()" ;;
-       esac
-   else
-     echo "'$1' is not a valid file"
-   fi
-}
+#function extract () {
+#  if [[ -f $1 ]]; then
+#    case "$1" in
+#      *.tar.bz2)   tar xjf "$1"     ;;
+#      *.tar.gz)    tar xzf "$1"     ;;
+#      *.bz2)       bunzip2 "$1"     ;;
+#      *.rar)       unrar e "$1"     ;;
+#      *.r00)       unrar e "$1"     ;;
+#      *.gz)        gunzip "$1"      ;;
+#      *.tar)       tar xf "$1"      ;;
+#      *.tbz2)      tar xjf "$1"     ;;
+#      *.tgz)       tar xzf "$1"     ;;
+#      *.zip)       unzip "$1"       ;;
+#      *.Z)         uncompress "$1"  ;;
+#      *.7z)        7z x "$1"        ;;
+#      *.001)       7z x "$1"        ;;
+#      *)     echo "'$1' cannot be extracted via extract()" ;;
+#       esac
+#   else
+#     echo "'$1' is not a valid file"
+#   fi
+#}
 
 #? zipf -> To create a ZIP archive of a folder
 function zipf () { zip -r "$1".zip "$1" ; }
