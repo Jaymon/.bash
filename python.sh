73c048e0eca97cf674244530aed6e5ba6fbe4fca
Jay Marcyes <jay@marcyes.com>
2017-12-18 11:26:06 -0700

    adds python 3 __cache__ dir removal to pycrm

--- python.sh
+++ python.sh
@@ -23,11 +23,14 @@ alias pyreg='python setup.py register'
 
 #? pycrm <PATH> -> remove all .pyc files at PATH, defaults to .
 function pycrm(){
-  if [ "$#" -eq 0 ]; then
-    find . -name '*.pyc' -delete
-  else
-    find $1 -name '*.pyc' -delete
+  d="."
+  if [ "$#" -gt 0 ]; then
+    d=$1
   fi
+
+  find $d -name '*.pyc' -delete
+  find $d -name '__pycache__' -type d -exec rm -r "{}" \;
+
 }
 alias rmpyc=pycrm
 
