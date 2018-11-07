a75353901467f85829874597cc412e1bf5644c8a
Jay Marcyes <jay@marcyes.com>
2018-11-06 17:24:53 -0700

    fixes a problem typegrep had when the paths had a space in them

--- search.sh
+++ search.sh
@@ -1,6 +1,7 @@
 
 #? typegrep TYPE PATTERN -> search only files with extension TYPE for PATTERN
 function typegrep() {
+  #set -x
   flags="-Rin"
   if [[ $2 =~ [A-Z] ]]; then
     flags="-Rn"
@@ -8,37 +9,60 @@ function typegrep() {
 
   # http://stackoverflow.com/a/8906229/5006
   if git -C . rev-parse  > /dev/null 2>&1; then
+
     # in order to filter the gitignored files we first just return the files that
     # have matches, then whittle those down and then print the matches to the screen
-    grep $flags --include=*.$1 --files-with-matches "$2" "${@:3}" . | git check-ignore -vn --stdin | grep "^::" | cut -d$'\t' -f2 | xargs grep -H --line-number --color "$2"
-  else
-    grep $flags --color --line-number --include=*.$1 "$2" "${@:3}" .
-  fi
+    grep $flags --include=*.$1 --files-with-matches "$2" "${@:3}" . \
+        | git check-ignore -vn --stdin \
+        | grep "^::" \
+        | cut -d$'\t' -f2 \
+        | tr '\n' '\0' \
+        | xargs -0 grep ${flags}H --line-number --color "$2"
 
+  else
+
+    grep $flags --color --line-number --include=*.$1 "$2" "${@:3}" .
+
+  fi
+  #set +x
 }
 
 
+#? txtgrep PATTERN -> search txt files for PATTERN
 function txtgrep() {
   typegrep txt "$@"
+  typegrep md "$@"
 }
 alias textgrep=txtgrep
 
 
+#? shgrep PATTERN -> search shell/bash files for PATTERN
 function shgrep() {
   typegrep sh "$@"
 }
 
 
+#? rbgrep PATTERN -> search ruby files for PATTERN
 function rbgrep() {
   typegrep rb "$@"
 }
 alias rubygrep=rbgrep 
 
+
+#? pygrep PATTERN -> search python files for PATTERN
 function pygrep() {
   # http://stackoverflow.com/a/3816747/5006 "$@" is handled as a special case by the shell. 
   # That is, "$@" is equivalent to "$1" "$2" ...
   typegrep py "$@" --exclude=*.pyc --exclude-dir="python2*" --exclude-dir="python3*"
 
+}
+
+
+#? iosgrep PATTERN -> search obj-c and swift files for PATTERN
+function iosgrep() {
+  typegrep m "$@"
+  typegrep h "$@"
+  typegrep swift "$@"
 }
 
 
